9f464c9f9b8de533eb3b833b9a7eecc66e27ab8b
ekaterina <zaitsevae1609@gmail.com>
2022-06-07 10:39:53 +0700

    home work

---
+++ Example/HomeWork1lection/Program.cs
@@ -0,0 +1,25 @@
+﻿int count = 0;
+int ferstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time = 0;
+int distance = 10000;
+
+while(distance > 10) {
+if(friend == 1) {
+time = distance / (ferstFriendSpeed + dogSpeed);
+}
+else
+{
+   time = distance / (secondFriendSpeed + dogSpeed); 
+}
+
+distance = distance - (ferstFriendSpeed + secondFriendSpeed) * time;
+
+count = count + 1;
+}
+
+Console.WriteLine("Собака пробежит " + count  + " раз");
+
+
