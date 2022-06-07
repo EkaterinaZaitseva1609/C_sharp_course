e6ee35dd69ed07d838135f77a5cfacb195286938
ekaterina <zaitsevae1609@gmail.com>
2022-06-07 12:21:29 +0700

    first ex

---
+++ Seminar 1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход
+//принимает число и выдаёт его квадрат (число
+//умноженное на само себя).
+
+
+/*System.Console.Write("Введите число ");
+int number = Int32.Parse(Console.ReadLine());
+int res = number * number;
+System.Console.WriteLine(res);
+*/
+System.Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()); 
+
+
+System.Console.WriteLine("Число:" + n);
+System.Console.WriteLine("Квадрат числа:" + n * n);
+
+
