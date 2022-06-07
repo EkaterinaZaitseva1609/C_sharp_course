8a77944912b68d2dd2e0622d36d86830d0e46099
ekaterina <zaitsevae1609@gmail.com>
2022-06-07 17:48:16 +0700

    ex last

---
+++ Seminar_1/Seminar_005/Program.cs
@@ -0,0 +1,16 @@
+﻿/*7. Напишите программу, которая принимает на вход
+трёхзначное число и на выходе показывает последнюю цифру
+этого числа. */
+
+System.Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine (num % 10);
+
+
+/*int index = 2;
+System.Console.WriteLine("Введите трехзначное число");
+string num = (Console.ReadLine());
+
+System.Console.WriteLine (num [2]);
+*/
