219757b7b2cb9d00b488662a42bb81fc162e72e5
ekaterina <zaitsevae1609@gmail.com>
2022-06-08 17:36:37 +0700

    Method

---
+++ Lecture_2/Ex_3/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 8, 2, 3, 4, 5, 7, 16, 8 };
+
+int n = array.Length;
+int find = 8;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+        index++;
+}
