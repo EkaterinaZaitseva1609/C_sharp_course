83231bee931c18b6b8f2748918513e52a99bf2e6
ekaterina <zaitsevae1609@gmail.com>
2022-06-10 15:12:12 +0700

    ex_3

---
+++ Seminar_2/Ex_3/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Seminar2Ex3
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Введите первое число");
+        int num1 = Int32.Parse(Console.ReadLine());
+        System.Console.WriteLine("Введите второе число");
+        int num2 = Int32.Parse(Console.ReadLine());
+if (num1 % num2 == 0)
+{
+    System.Console.WriteLine(num1 + "," + num2 + " -> кратно");
+}
+else
+{
+    System.Console.WriteLine(num1 + "," + num2 + " -> не кратно, " + "остаток " + num1 % num2);
+}
+    }
+
+
+}
