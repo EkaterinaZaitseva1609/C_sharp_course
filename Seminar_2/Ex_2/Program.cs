c6a1bb97389f2a7c66f8a1884b55248b20d8fdc4
ekaterina <zaitsevae1609@gmail.com>
2022-06-10 15:00:41 +0700

    ex_2

---
+++ Seminar_2/Ex_2/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Seminar2Ex2
+{
+    public static void Main()
+    {
+        int x1 = new Random().Next(100, 1000);
+        int x2 = x1 / 100;
+        int x3 = x1 % 10;
+        
+            System.Console.WriteLine( x1 + " -> " + x2 + x3 );
+    
+
+    }
+}
+
