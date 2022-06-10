59eb6136d80bb08f4c805579e1acb1bf6f3e5b16
ekaterina <zaitsevae1609@gmail.com>
2022-06-10 13:27:43 +0700

    ex_1

--- Seminar_2/Ex_1/Program.cs
+++ Seminar_2/Ex_1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ public class Seminar2Ex1
+ {
+    public static void Main()
+    {
+        int num = new Random().Next(10, 100);
+int n1 = num % 10;
+int n2 = num / 10;
+
+
+        if (n1 > n2)
+        {
+            System.Console.WriteLine(num + " -> " + n1);
+        }
+        else
+        {
+            System.Console.WriteLine(num + " -> " + n2);
+        }
+
+
+     }
+ }
