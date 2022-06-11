23277f5832b6464235ec8d2301e9032c8d7003ed
ekaterina <zaitsevae1609@gmail.com>
2022-06-11 12:48:44 +0700

    ex_4

---
+++ Seminar_2/Ex_4/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Seminar2Ex4
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Введите любое число");
+        int num = Int32.Parse(Console.ReadLine());
+
+        if (num % 7 == 0 && num % 23 == 0)
+        {
+            System.Console.WriteLine(num + " -> " + "yes");
+        }
+        else
+        {
+            System.Console.WriteLine(num + " -> " + "No");
+        }
+    }
+}
