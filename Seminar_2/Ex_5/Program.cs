6aca6c262b19ba8509d4785076bccf2fd1a9c56f
ekaterina <zaitsevae1609@gmail.com>
2022-06-11 13:55:54 +0700

    Ex_5

---
+++ Seminar_2/Ex_5/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Seminar2Ex4
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Введите трехзначное число");
+        int num = Int32.Parse(Console.ReadLine());
+
+        if (num < 100)
+        {
+            System.Console.WriteLine("Нет третьего числа");
+        }
+        else
+        {
+            while (num >= 1000)
+            {
+                num = num / 10;
+            }
+            int num3 = num % 10;
+            System.Console.WriteLine(num3);
+
+        }
+
+    }
+
+}
+
