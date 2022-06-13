eb229d51bcac5ab2b63cdaf14dbcc396e535057c
ekaterina <zaitsevae1609@gmail.com>
2022-06-13 19:21:18 +0700

    home

---
+++ test/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Test1
+{
+    public static void Main()
+    {
+        byte[] nums = { 5, 6, 7, 8, 3 };
+        byte res1 = Summa(nums);
+        System.Console.WriteLine("Результат " + res1);
+    }
+
+    public static byte Summa(byte[] digits)
+    {
+        byte summ = 0;
+        foreach (byte el in digits)
+            summ += el;
+
+        return summ;
+
+    }
+}
