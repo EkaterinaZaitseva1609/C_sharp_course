43c4e88910b89c4b6ea3157bcc309da16392d5fe
ekaterina <zaitsevae1609@gmail.com>
2022-06-14 15:54:26 +0700

    Ex-1

---
+++ Seminar_3/Ex_1/Program.cs
@@ -0,0 +1,39 @@
+﻿/*17. Напишите программу, которая принимает на вход
+координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+номер четверти плоскости, в которой находится эта
+точка.
+*/
+
+
+
+public class Seminar2Ex4
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Введите x");
+        int x = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("Введите y");
+        int y = int.Parse(Console.ReadLine());
+
+        if (x > 0 && y > 0)
+        {
+            System.Console.WriteLine("Первая плоскость");
+        }
+        else if (x < 0 && y > 0)
+        {
+            System.Console.WriteLine("Вторая плоскость");
+        }
+        else if (x < 0 && y < 0)
+        {
+            System.Console.WriteLine("Третья плоскость");
+        }
+        else if (x > 0 && y < 0)
+        {
+            System.Console.WriteLine("Четвертая плоскость");
+        }
+        else {
+            System.Console.WriteLine("Координаты не принадлежат плоскости");
+        }
+    }
+}
