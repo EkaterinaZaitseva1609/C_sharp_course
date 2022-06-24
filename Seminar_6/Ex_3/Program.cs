72765ef6ac8cf198e12e679e8d9a969ec4bc6724
ekaterina <zaitsevae1609@gmail.com>
2022-06-24 15:53:41 +0700

    перевод из десятичной в двоичную

---
+++ Seminar_6/Ex_3/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 42: Напишите программу, которая будет преобразовывать
+десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+ */
+
+
+System.Console.WriteLine("Введите число");
+int num = Int32.Parse(Console.ReadLine());
+int res = 0;
+string result = string.Empty; 
+while (num > 0)
+{
+    res = num % 2;
+    num = num / 2;
+    result = res + result; 
+}
+
+System.Console.WriteLine(result);
+
+
+
