6f47098ea8d128caa8a082a8b025e8b558fd600b
ekaterina <zaitsevae1609@gmail.com>
2022-06-30 17:52:27 +0700

    hackerRanc

---
+++ Seminar_7/Ex_5_recursiya/Program.cs
@@ -0,0 +1,14 @@
+﻿//рекурсия 
+
+int Rec(int num, int b)
+{
+    if (num <= 0) //условие остановки
+    {
+        return 1; // пишем для того, чтобы функция прекратилась (return - стой)
+    }
+    System.Console.WriteLine("рекурсия" + num);
+    return Rec(num - 1, b) * b; //рекурсивыный вызов, вызываем рекурсивную функцию в себя 
+}
+
+System.Console.WriteLine(Rec(4, 5));
+ // 5 раз написали "рекурсия "
--- dop/Ex_1/Program.cs
+++ dop/Ex_1/Program.cs
@@ -1,15 +1,79 @@
-﻿//Работа с файлами и строками 
+﻿/*void staircase(int n)
+{
 
-string word = "Hello";
-word += "!";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j >= i)
+            {
+                System.Console.Write("#");
+            }
+            else
+            {
+                System.Console.Write(" ");
+            }
 
+        }
+        System.Console.WriteLine("");
+    }
 
+}
+staircase(4);
+*/
+/*
+void staircase(int n)
+{
 
-//System.Console.WriteLine(word.Length);
-//word = String.Concat(word, "!!");
-//System.Console.WriteLine(String.Compare(word, "Heoo!!!!"));
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j >= i)
+            {
+                System.Console.Write("#");
+            }
+            else
+            {
+                System.Console.Write(" ");
+            }
 
-string people = "Alex, Bob, John"; 
-string[] names = people.Split(new char[]{','});
-foreach(string el in names)
-System.Console.WriteLine(el);
+        }
+        System.Console.WriteLine("");
+    }
+
+}
+staircase(4);
+*/
+
+void miniMaxSum(List<int> arr)
+{
+    long summ = 0;
+     long max = 0;
+     long min = Int32.MaxValue;
+    for (int j = 0; j < arr.Count; j++)
+    {
+       
+        for (int i = 0; i < arr.Count; i++)
+        {
+            if (j != i)
+            {   
+                summ = summ + arr[i];
+            }
+        }
+        System.Console.WriteLine(summ);
+        if (summ > max)
+            {
+                max = summ;
+            }
+            if (summ < min)
+            {
+                min = summ;
+            }
+        summ = 0;
+    }
+System.Console.WriteLine(min + " " + max);
+}
+List<int> mass = new List<int>() { 1, 2, 3, 4, 5 };
+miniMaxSum(mass);
+
