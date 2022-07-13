1894f70849eba601245bf21ad326ad0ae38623ce
ekaterina <zaitsevae1609@gmail.com>
2022-07-13 11:41:44 +0700

    контрольная

--- Practis/Program.cs
+++ Practis/Program.cs
@@ -1,3 +1,40 @@
 ﻿// Практикум
 
 
+string Print(string[] array)
+{
+    int length = array.Length;
+    string output = String.Empty;
+    for (int i = 0; i < length; i++)
+    {
+        output += (array[i] + " ");
+    }
+    return output;
+}
+
+
+string[] GetWord(string[] array, int n)
+{
+    string[] outputArray = new string[array.Length];
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n)
+        {
+            outputArray[index] = array[i];
+            index++;
+        }
+    }
+    return outputArray;
+}
+
+
+System.Console.WriteLine("Введит число N");
+int n = int.Parse(Console.ReadLine());
+
+string[] array = { "hello", "123", "world", "Denmark", "yes", "min" };
+string[] result = GetWord(array, n);
+string output = Print(result);
+System.Console.WriteLine(output);
+File.WriteAllText("output.txt", output);
+
---
+++ dop/ Mass/Ex_2/Program.cs
@@ -0,0 +1,31 @@
+﻿List<int> arr = new List<int>();
+arr.Add(-4);
+arr.Add(3);
+arr.Add(-9);
+arr.Add(0);
+arr.Add(4);
+arr.Add(1);
+
+double sum1 = 0;
+double sum2 = 0;
+double sum3 = 0;
+foreach(double a in arr)
+{
+    if(a > 0)
+    {
+    sum1 = sum1 + 1;
+    }
+    if (a < 0)
+    {
+    sum2 = sum2 + 1;
+    }
+    if (a == 0)
+     sum3 = sum3 + 1;
+}
+
+System.Console.WriteLine (Math.Round(sum1 / arr.Count, 6));
+
+System.Console.WriteLine (Math.Round(sum2 / arr.Count, 6));
+
+System.Console.WriteLine (Math.Round(sum3 / arr.Count, 6));
+
